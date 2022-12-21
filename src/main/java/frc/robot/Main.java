fc11e670fd078019bd49324221f43795a637aaf3
Matthew <92895537+Hehehsxhhsbs@users.noreply.github.com>
2022-12-21 18:58:35 -0500

    Practice

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -19,6 +19,8 @@ public final class Main {
    *
    * <p>If you change your main robot class, change the parameter type.
    */
+
+   //SDKjfljdsfg;sdj;lkzjf;kndmzlkfjg
   public static void main(String... args) {
     RobotBase.startRobot(Robot::new);
   }
