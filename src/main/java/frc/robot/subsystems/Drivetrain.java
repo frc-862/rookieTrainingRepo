c8e0dd69626788185706bf73d8e4088399aea610
im-a-robo <nmdas05x@gmail.com>
2022-12-21 19:38:46 -0500

    i made the robot fly ;) #1

---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,34 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.constants.RobotMap;
+
+public class Drivetrain extends SubsystemBase {
+  private VictorSP left;
+  private VictorSP right;
+
+  public Drivetrain() {
+    left = new VictorSP(RobotMap.LEFT);
+    right = new VictorSP(RobotMap.RIGHT);
+
+    left.setInverted(true);
+
+    // Funny robot fly now
+    CommandScheduler.getInstance().registerSubsystem(this);
+  }
+
+  public void setPower(double leftPower, double rightPower) {
+    left.set(leftPower);
+    right.set(rightPower);
+  }
+
+  public void stop() {
+    setPower(0d, 0d);
+  }
+}
