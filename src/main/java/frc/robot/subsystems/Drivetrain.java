f9def38525200b09e1582a5528cf88eaefdb9d82
im-a-robo <nmdas05x@gmail.com>
2022-12-21 19:06:15 -0500

    the message

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -19,6 +19,7 @@ public class Drivetrain extends SubsystemBase {
 
     left.setInverted(true);
 
+    // Register this subsystem with the CommandScheduler
     CommandScheduler.getInstance().registerSubsystem(this);
   }
 
